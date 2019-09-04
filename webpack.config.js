efbe540ff68fc674804830167dfc42eb0250a420
Brett Cunningham <bwcunninghamii@gmail.com>
2019-09-04 14:24:46 -0400

    enabled webpack to created two bundles, one for client and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,118 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './view/src/index.js',
-  output: {
-    path: path.join(__dirname, './view/public'),
-    filename: 'index_bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.js$/, /\.jsx?$/], 
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
+module.exports = [
+  {
+    entry: './view/src/components/client.js',
+    output: {
+      path: path.join(__dirname, './view/public'),
+      filename: 'client_bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx?$/], 
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: './view/src/index.html'
+      })
     ]
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      template: './view/src/index.html'
-    })
-  ]
-}
+  }, {
+    entry: './view/src/components/server.js',
+    target: 'node',
+    output: {
+      path: path.join(__dirname, './view/public'),
+      filename: 'server_bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx?$/], 
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: './view/src/index.html'
+      })
+    ]
+  }
+]
+// Original
+// module.exports = {
+//   entry: './view/src/index.js',
+//   output: {
+//     path: path.join(__dirname, './view/public'),
+//     filename: 'index_bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: [/\.js$/, /\.jsx?$/], 
+//         loader: 'babel-loader',
+//         exclude: /node_modules/
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new htmlWebpackPlugin({
+//       template: './view/src/index.html'
+//     })
+//   ]
+// }
+
+// server side rendering webpack
+
+/**
+ * module.exports = [
+  {
+    entry: './view/src/components/client.js',
+    output: {
+      path: path.join(__dirname, './view/public'),
+      filename: 'client_bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx?$/], 
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: './view/src/index.html'
+      })
+    ]
+  }, {
+    entry: './view/src/components/server.js',
+    target: 'node',
+    output: {
+      path: path.join(__dirname, './view/public'),
+      filename: 'server_bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx?$/], 
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: './view/src/index.html'
+      })
+    ]
+  }
+]
+ */
